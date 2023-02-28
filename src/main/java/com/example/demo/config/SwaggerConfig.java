38124495eb10413747d750ff3dd9cdd7533e1972
mohamed <mohamed@gmail.com>
2023-02-28 13:55:58 +0100

    commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//	private String hostUrl;
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//		.host(hostUrl)
+//		.groupName("Spring Actuator")
+//		.select()
+//		.apis(RequestHandlerSelectors.any())
+//		.paths(PathSelectors.any())
+//		.build();
+//	}
+//}
